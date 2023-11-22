bfa4a70f721b668c5dc855a9978754126ca6336a
Winn <winnieee.lam00@gmail.com>
2023-11-21 22:35:21 -0500

    completed template code

---
+++ template copy/playlistScript.js
@@ -0,0 +1,168 @@
+const sunny = [
+  {
+    album: "Dangerously In Love",
+    name: "Crazy In Love (feat. Jay Z)",
+    album_art:
+      "https://i.scdn.co/image/ab67616d0000b27345680a4a57c97894490a01c1",
+    spotify_url:
+      "https://open.spotify.com/embed/track/5IVuqXILoxVWvWEPm82Jxr?utm_source=generatorr",
+  },
+  {
+    album: "RENAISSANCE",
+    name: "CUFF IT",
+    album_art:
+      "https://i.scdn.co/image/ab67616d0000b2730e58a0f8308c1ad403d105e7",
+    spotify_url:
+      "https://open.spotify.com/embed/track/1xzi1Jcr7mEi9K2RfzLOqS?utm_source=generator",
+  },
+  {
+    album: "RENAISSANCE",
+    name: "ALIEN SUPERSTAR",
+    album_art:
+      "https://i.scdn.co/image/ab67616d0000b2730e58a0f8308c1ad403d105e7",
+    spotify_url:
+      "https://open.spotify.com/embed/track/1Hohk6AufHZOrrhMXZppax?utm_source=generator",
+  },
+  {
+    album: "B'Day Deluxe Edition",
+    name: "Deja Vu (feat. Jay-Z)",
+    album_art:
+      "https://i.scdn.co/image/ab67616d0000b273026e88f624dfb96f2e1ef10b",
+    spotify_url:
+      "https://open.spotify.com/embed/track/423EcxblW9F4nnQkqcqMlK?utm_source=generator",
+  },
+  {
+    album: "Lemonade",
+    name: "All Night",
+    album_art:
+      "https://i.scdn.co/image/ab67616d0000b27389992f4d7d4ab94937bf9e23",
+    spotify_url:
+      "https://open.spotify.com/embed/track/7oAuqs6akGnPU3Tb00ZmyM?utm_source=generator",
+  },
+  {
+    album: "B'Day Deluxe Edition",
+    name: "Beautiful Liar (with Shakira)",
+    album_art:
+      "https://i.scdn.co/image/ab67616d0000b273026e88f624dfb96f2e1ef10b",
+    spotify_url:
+      "https://open.spotify.com/embed/track/2HWWNoWEEEECwZhAiLg7ib?utm_source=generator",
+  },
+  {
+    album: "4",
+    name: "Best Thing I Never Had",
+    album_art:
+      "https://i.scdn.co/image/ab67616d0000b273ff5429125128b43572dbdccd",
+    spotify_url:
+      "https://open.spotify.com/embed/track/3lBRNqXjPp2j3JMTCXDTNO?utm_source=generator",
+  },
+  {
+    album: "4",
+    name: "Countdown",
+    album_art:
+      "https://i.scdn.co/image/ab67616d0000b273ff5429125128b43572dbdccd",
+    spotify_url:
+      "https://open.spotify.com/embed/track/3axkNosdVQLZiq1HakuGhc?utm_source=generator",
+  },
+];
+
+const cloudy = [
+  {
+    album: "RENAISSANCE",
+    name: "VIRGO'S GROOVE",
+    album_art:
+      "https://i.scdn.co/image/ab67616d0000b2730e58a0f8308c1ad403d105e7",
+    spotify_url:
+      "https://open.spotify.com/embed/track/0Fl4eWzVaMUOdXcOrj6F1q?utm_source=generatorr",
+  },
+  {
+    album: "AMERICA HAS A PROBLEM",
+    name: "AMERICA HAS A PROBLEM (feat. Kendrick Lamar)",
+    album_art:
+      "https://i.scdn.co/image/ab67616d0000b27378643be90cd7c5ec75675855",
+    spotify_url:
+      "https://open.spotify.com/embed/track/6l8mgVN9Xf1hiDIFGA6CTE?utm_source=generatorr",
+  },
+  {
+    album: "Lemonade",
+    name: "Formation",
+    album_art:
+      "https://i.scdn.co/image/ab67616d0000b27389992f4d7d4ab94937bf9e23",
+    spotify_url:
+      "https://open.spotify.com/embed/track/6g0Orsxv6glTJCt4cHsRsQ?utm_source=generatorr",
+  },
+  {
+    album: "BEYONCÉ [Platinum Edition]",
+    name: "Drunk in Love (feat. Jay-Z)",
+    album_art:
+      "https://i.scdn.co/image/ab67616d0000b2730d1d6e9325275f104f8e33f3",
+    spotify_url:
+      "https://open.spotify.com/embed/track/6jG2YzhxptolDzLHTGLt7S?utm_source=generatorr",
+  },
+  {
+    album: "BEYONCÉ [Platinum Edition]",
+    name: "Partition",
+    album_art:
+      "https://i.scdn.co/image/ab67616d0000b2730d1d6e9325275f104f8e33f3",
+    spotify_url:
+      "https://open.spotify.com/embed/track/5hgnY0mVcVetszbb85qeDg?utm_source=generatorr",
+  },
+  {
+    album: "Lemonade",
+    name: "6 Inch (feat. The Weekend)",
+    album_art:
+      "https://i.scdn.co/image/ab67616d0000b27389992f4d7d4ab94937bf9e23",
+    spotify_url:
+      "https://open.spotify.com/embed/track/0pxNyJJiL2zZZ5GgJ4JhsQ?utm_source=generatorr",
+  },
+];
+
+const rainy = [
+  {
+    album: "B'Day Deluxe Edition",
+    name: "Irreplaceable",
+    album_art:
+      "https://i.scdn.co/image/ab67616d0000b273026e88f624dfb96f2e1ef10b",
+    spotify_url:
+      "https://open.spotify.com/embed/track/6RX5iL93VZ5fKmyvNXvF1r?utm_source=generator",
+  },
+  {
+    album: "I AM...SASHA FIERCE",
+    name: "Single Ladies (Put a Ring on It)",
+    album_art:
+      "https://i.scdn.co/image/ab67616d0000b273801c4d205accdba0a468a10b",
+    spotify_url:
+      "https://open.spotify.com/embed/track/5R9a4t5t5O0IsznsrKPVro?utm_source=generatorr",
+  },
+  {
+    album: "I AM...SASHA FIERCE",
+    name: "Broken-Hearted Girl",
+    album_art:
+      "https://i.scdn.co/image/ab67616d0000b273801c4d205accdba0a468a10b",
+    spotify_url:
+      "https://open.spotify.com/embed/track/31acMiV67UgKn1ScFChFxo?utm_source=generatorr",
+  },
+  {
+    album: "RENAISSANCE",
+    name: "BREAK MY SOUL",
+    album_art:
+      "https://i.scdn.co/image/ab67616d0000b2730e58a0f8308c1ad403d105e7",
+    spotify_url:
+      "https://open.spotify.com/embed/track/5pyoxDZ1PX0KxBxiRVxA4U?utm_source=generatorr",
+  },
+  {
+    album: "The Fame Monster (Deluxe Edition)",
+    name: "Telephone (with Lady Gaga)",
+    album_art:
+      "https://i.scdn.co/image/ab67616d0000b273bfd247438980173d32fd5ba8",
+    spotify_url:
+      "https://open.spotify.com/embed/track/7rl7ao5pb9BhvAzPdWStxi?utm_source=generatorr",
+  },
+  {
+    album: "Lemonade",
+    name: "Freedom (feat. Kendrick Lamar)",
+    album_art:
+      "https://i.scdn.co/image/ab67616d0000b27389992f4d7d4ab94937bf9e23",
+    spotify_url:
+      "https://open.spotify.com/embed/track/7aBxcRw77817BrkdPChAGY?utm_source=generatorr",
+  },
+];
---
+++ template copy/script.js
@@ -0,0 +1,101 @@
+const weather = {
+  sunny: ["sunny", "clear"],
+  rainy: ["mist", "fog", "rain", "drizzle", "thunder"],
+  cloudy: ["cloudy", "overcast"],
+};
+
+let playlist = [];
+
+const prevSong = document.querySelector(".prev-btn");
+const nextSong = document.querySelector(".next-btn");
+const albumArt = document.querySelector(".album-art");
+const albumTitle = document.querySelector(".album-title");
+const texts = document.querySelectorAll(".text");
+const songNum = document.querySelector(".song-num");
+const songName = document.querySelector(".song-name");
+const spotifyWidget = document.querySelector(".spotify-widget iframe");
+const forecast = document.querySelector(".forecast span");
+const weatherIcon = document.querySelector(".weather-icon");
+
+function handlePlayer() {
+  prevSong.addEventListener("click", () => handleSongControl(-1));
+  nextSong.addEventListener("click", () => handleSongControl(1));
+  albumArt.addEventListener("animationend", () => {
+    albumArt.classList.remove("album-transition");
+    for (const text of texts) {
+      text.classList.add("show-texts");
+    }
+  });
+
+  const handleSongControl = (direction) => {
+    const songPosition = index + direction;
+    if (songPosition >= 0 && songPosition < playlist.length) {
+      updateDisplay((index += direction));
+    }
+  };
+
+  let index = 0;
+  const updateDisplay = (index) => {
+    for (const text of texts) {
+      text.classList.remove("show-texts");
+    }
+
+    const number = index + 1;
+    songNum.innerText = number >= 10 ? number + "." : `0${number}.`;
+
+    const song = playlist[index];
+    albumArt.src = song.album_art;
+    albumTitle.textContent = song.album;
+    songName.textContent = song.name;
+    spotifyWidget.src = song.spotify_url;
+
+    if (index === 0) {
+      prevSong.classList.add("hide-arrow");
+    } else {
+      prevSong.classList.remove("hide-arrow");
+    }
+
+    if (index === playlist.length - 1) {
+      nextSong.classList.add("hide-arrow");
+    } else {
+      nextSong.classList.remove("hide-arrow");
+    }
+
+    albumArt.classList.add("album-transition");
+  };
+
+  updateDisplay(index);
+}
+
+fetch(
+  "http://api.weatherapi.com/v1/current.json?key=8251437c5c1f45afab802447231810&q=toronto&aqi=no"
+)
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    // get weather condition from api
+    const condition = data.current.condition.text;
+    forecast.textContent = condition;
+    weatherIcon.src = "https:" + data.current.condition.icon;
+    // loop through possible weather conditions
+    for (const [key, value] of Object.entries(weather)) {
+      for (const weather_type of value) {
+        // the condition holds a key word from our dictionary
+        if (condition.toLowerCase().includes(weather_type)) {
+          // set playlist based on weather
+          if (key == "sunny") {
+            playlist = sunny;
+          } else if (key == "rainy") {
+            playlist = rainy;
+          } else {
+            playlist = cloudy;
+          }
+        }
+      }
+    }
+    handlePlayer();
+  })
+  .catch(function (err) {
+    console.warn("Something went wrong.", err);
+  });
